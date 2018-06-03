7fe4e0dd0e7109d85091d6420d8da4ae20c683f3
Oumaima <oumaimaLam@users.noreply.github.com>
2018-06-03 23:18:26 +0000

    + comments

--- DC_SingleMotor.ino
+++ DC_SingleMotor.ino
@@ -1,9 +1,12 @@
 #include <AFMotor.h>
-AF_DCMotor motor1(1);
 
+//declare motors on the shield
+AF_DCMotor motor1(1);
 
 void setup() {
   // put your setup code here, to run once:
+  // min operational speed for bilge pumps 150 -- max speed 250
+  
   motor4.setSpeed(250);
   motor4.run(RELEASE);
 
@@ -11,9 +14,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Forward motion -- could be changed by switching cables in the motor shield
   motor4.run(FORWARD);
   delay(3000);
   
+  //Backward motion -- could be changed by switching cables in the motor shield
   motor4.run(BACKWARD);
   delay(3000);
 
